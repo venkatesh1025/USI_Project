4db3543f4c22bca9d27a91363d982cea0f5d1e08
ganni1338 <92354667+ganni1338@users.noreply.github.com>
2022-03-18 12:21:13 +0530

    Update LoginModel.cs

--- Models/LoginModel.cs
+++ Models/LoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,13 @@ namespace USI_Project.Models
 {
     public class LoginModel
     {
+        [Required(ErrorMessage="enter valid UserName:")]
+        [Display(Name ="UserName:")]
         public string UserName { get; set; }
+        
+        [Required(ErrorMessage ="enter password:")]
+        [Display(Name ="Password:")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
